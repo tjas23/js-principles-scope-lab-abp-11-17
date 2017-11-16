2991a282a807a9d90a6a8a918fffcde509caf011
Thomas Jascur <tjascur@yahoo.com>
2017-11-16 05:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(customerName) {
+  return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
