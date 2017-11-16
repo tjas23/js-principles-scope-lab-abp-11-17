4ec5728148b60e6c5ad2e1ef1a6ecd2a0bd24244
Thomas Jascur <tjascur@yahoo.com>
2017-11-16 05:50:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var customerName = 'bob';
 
 function upperCaseCustomerName(customerName) {
-  return customerName.toUpperCase()
+  customerName = customerName.toUpperCase;
+  return customerName
 }
 
 function setBestCustomer() {
